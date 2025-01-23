21db52bbee84caf75bbd7d06af87b3aed02559da
tipalsyahhh <tipalsyah123@gmail.com>
2025-01-23 21:43:09 +0700

    dasboard

--- scrip.js
+++ scrip.js
@@ -2,33 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const btnMenu = document.querySelector(".btn-menu");
     const navigationPage = document.querySelector(".navigation-page");
     const closeBtn = document.querySelector(".close-btn");
-
-    // Fungsi untuk membuka navigasi
     function openNavigation() {
         navigationPage.classList.add("active");
     }
-
-    // Fungsi untuk menutup navigasi
     function closeNavigation() {
         navigationPage.classList.remove("active");
     }
-
-    // Event listener untuk tombol menu
     btnMenu.addEventListener("click", function (event) {
-        event.stopPropagation(); // Mencegah klik pada tombol menutup navigasi
+        event.stopPropagation(); 
         openNavigation();
     });
-
-    // Event listener untuk tombol tutup
     closeBtn.addEventListener("click", function (event) {
-        event.stopPropagation(); // Mencegah klik pada tombol tutup memicu event luar
+        event.stopPropagation(); 
         closeNavigation();
     });
-
-    // Event listener untuk klik di luar navigasi
     document.addEventListener("click", function (event) {
         if (!navigationPage.contains(event.target) && !btnMenu.contains(event.target)) {
-            // Jika klik terjadi di luar navigasi dan tombol menu
             closeNavigation();
         }
     });
